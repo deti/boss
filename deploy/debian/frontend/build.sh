7faf14987ba0479a0b3776684848807f422db7ba
Anton Tagunov <anton.tagunov@yandex.ru>
2015-12-28 15:34:15 +0300

    BOSS-1970 added creating frontend rpm

---
+++ deploy/debian/frontend/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+
+NUMBER=${BUILD_NUMBER-0}
+
+# full path to deploy dir
+BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$BUILD_DIR/root"
+BOSS_ROOT="$ROOT/opt/boss"
+PROJECT_ROOT="$( cd "$BUILD_DIR/../../../" && pwd )"
+FRONTEND="$PROJECT_ROOT/frontend/admin"
+
+
+cd $FRONTEND
+
+npm install
+bower --config.interactive=false install --allow-root
+npm install
+npm run compile
+
+
+rm -rf $BOSS_ROOT/*
+mkdir -p $BOSS_ROOT
+
+mkdir -p $ROOT/DEBIAN
+cp $BUILD_DIR/DEBIAN/* $ROOT/DEBIAN
+sed -e "s/bossfrontendadmin\ (0\.0\.1)/bossfrontend\ (0\.0\.$BUILD_NUMBER)/" < "$BUILD_DIR/changelog" > "$ROOT/DEBIAN/changelog"
+sed -e "s/version:\ 0\.0\.1/version:\ 0\.0\.$BUILD_NUMBER/" < $BUILD_DIR/control > "$ROOT/DEBIAN/control"
+
+
+mkdir -p $ROOT/usr/share/boss_admin/
+cp $FRONTEND/bin/admin $ROOT/usr/share/boss_admin/
+
+
+cd $BUILD_DIR
+dpkg -b $ROOT
